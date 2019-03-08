0e4125d03a092f15a45115423a9aea6af46018f6
leanhduc123 <48117340+leanhduc123@users.noreply.github.com>
2019-03-08 14:42:24 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -20,6 +20,20 @@ int main(int argc, char* argv[])
 
     // Your drawing code here
     // use SDL_RenderPresent(renderer) to show it
+    SDL_RenderClear(renderer);
+
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);   // white
+    SDL_RenderDrawPoint(renderer, SCREEN_WIDTH/2, SCREEN_HEIGHT/2);
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);   // red
+    SDL_RenderDrawLine(renderer, 100, 100, 200, 200);
+    SDL_Rect filled_rect;
+    filled_rect.x = SCREEN_WIDTH - 400;
+    filled_rect.y = SCREEN_HEIGHT - 150;
+    filled_rect.w = 320;
+    filled_rect.h = 100;
+    SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255); // green
+    SDL_RenderFillRect(renderer, &filled_rect);
+    SDL_RenderPresent(renderer);
 
 	waitUntilKeyPressed();
     quitSDL(window, renderer);
